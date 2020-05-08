b2c7f6f05deb3ed5681e9d1b5aba79b82bce2d13
Alvaro Aspee <aspeealvaro@gmail.com>
2020-05-07 22:01:35 -0400

    Updates

---
+++ precache-manifest.dda8ee451b44406025c11d21f862ceb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442bd5b0596d94d3d91b56dfde265d09",
+    "url": "/React-my-app/index.html"
+  },
+  {
+    "revision": "a76683fbad62ca891b30",
+    "url": "/React-my-app/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/React-my-app/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-my-app/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76683fbad62ca891b30",
+    "url": "/React-my-app/static/js/main.8fd11f58.chunk.js"
+  },
+  {
+    "revision": "c2949142c67e067f9aa7",
+    "url": "/React-my-app/static/js/runtime-main.7382cccd.js"
+  }
+]);
